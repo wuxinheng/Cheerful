6af0ebdcc633ca1b01134db464e99a3f71f52dc1
吴新恒 <17629957093@163.com>
2022-10-31 13:49:01 +0800

    Edit Project Name

--- Cheerful.Test/RequstApiTest.cs
+++ Cheerful.Test/RequstApiTest.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.Net;
 using System.Net.Http.Headers;
 
-namespace Happy.Test
+namespace Cheerful.Test
 {
     [TestClass]
     public class RequstApiTest
---
+++ Cheerful/RequstApi.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace Cheerful
+{
+  public static class RequstApi
+  {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="httpRequestMessage"></param>
+    /// <returns></returns>
+    public static async Task<HttpContent> PostAsync(HttpRequestMessage httpRequestMessage)
+    {
+      HttpContent? httpContent;
+      var client = new HttpClient();
+      using (var response = await client.SendAsync(httpRequestMessage))
+      {
+        response.EnsureSuccessStatusCode();
+        httpContent = response.Content;
+      }
+      return httpContent;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public static byte[] DownloadToByte(string url)
+    {
+      WebClient webClient = new WebClient();
+      return webClient.DownloadData(url);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public static Stream DownloadToStream(string url)
+    {
+      WebClient webClient = new WebClient();
+      var bytes = webClient.DownloadData(url);
+      return new MemoryStream(bytes);
+    }
+
+  }
+}
